b110c565a1cf4e5cab010e3be515fd9e85166e80
dudgns2824 <dudgns2824@gmail.com>
2025-03-24 03:09:22 +0900

    처방코드 중복 체크 및 생성 로직 추가 command service는 어차피 트랜잭션 어노테이션이 필수이므로 어노테이션 추가

--- domains/domain-prescription-client/src/main/kotlin/beyondMedicine/com/backend/domains/prescription/util/PrescriptionCodeUtil.kt
+++ domains/domain-prescription-client/src/main/kotlin/beyondMedicine/com/backend/domains/prescription/util/PrescriptionCodeUtil.kt
@@ -1,10 +1,13 @@
 package beyondMedicine.com.backend.domains.prescription.util
 
 object PrescriptionCodeUtil {
+
     const val prescriptionCodeAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     const val prescriptionCodeNumber = "0123456789"
+
     fun createPrescriptionCode() = (buildString {
         repeat(4) { append(prescriptionCodeAlphabet.random()) }
         repeat(4) { append(prescriptionCodeNumber.random()) }
     }).toList().shuffled().joinToString()
+
 }
